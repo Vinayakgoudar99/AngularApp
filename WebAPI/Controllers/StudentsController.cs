2418d36b9f1cf3a542f904c1bd7aaaaba644dad5
Vinayakgoudar99 <120566051+Vinayakgoudar99@users.noreply.github.com>
2023-02-14 18:03:23 +0530

    Add files via upload

---
+++ WebAPI/Controllers/StudentsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.DataComponents;
+
+namespace WebAPI.Controllers
+{
+    
+    public class StudentsController : ApiController
+    {
+        private readonly Entities data = new Entities();
+        [ResponseType(typeof(List<Student>))]
+        public async Task<IHttpActionResult> GetAllStudents()
+        {
+            var students=data.Students.ToList();
+            return await Task.Run<IHttpActionResult>(() => Ok(students));
+        }
+        [Route("api/Students/{StudentId}")]
+        public async Task<IHttpActionResult> GetStudent(int StudentId)
+        {
+            var students = data.Students.ToList();
+            var student = students.Find(e=>e.StudentId==StudentId);
+            return await Task.Run<IHttpActionResult>(() => Ok(student));
+
+        }
+        [HttpPost]
+        //[ResponseType(typeof(Student))]
+        public async Task<IHttpActionResult> AddStudent(Student student)
+        {
+            var std = new Student
+            {
+                StudentName=student.StudentName,
+                StudentAddress=student.StudentAddress,
+                MobileNo=student.MobileNo
+            };
+            data.Students.Add(std);
+            data.SaveChanges();
+            return await Task.Run<IHttpActionResult>(() => Ok());
+        }
+       
+
+    }
+}
---
+++ WebAPI/DataComponents/StudentVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.DataComponents
+{
+    public class StudentVM
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string StudentAddress { get; set; }
+        public long MobileNo { get; set; }
+    }
+}
